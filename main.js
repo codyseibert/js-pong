374db4912ccf8591cb015ef99589d2c1aa40e432
Cody Seibert <cseibert113@gmail.com>
2017-11-02 17:16:58 -0400

    added functionality for handling collisions and reflections

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 const $pong = $('#pong');
 const $playerPadel = $('#player-padel');
 const $aiPadel = $('#ai-padel');
+const $ball = $('#ball');
+
+const UP_LEFT = -3 * Math.PI / 4;
+const UP_RIGHT = - Math.PI / 4;
+const DOWN_LEFT = 3 * Math.PI / 4;
+const DOWN_RIGHT = Math.PI / 4;
 
 const aiPadel = {
   direction: 1,
   SPEED: 3,
   top: 0
+}
+
+const ball = {
+  top: 240,
+  left: 460,
+  angle: UP_RIGHT,
+  speed: 1.5
 }
 
 $pong.mousemove(function (evt) {
@@ -19,6 +32,68 @@ $pong.mousemove(function (evt) {
 });
 
 function update() {
+  updateBall();
+  updateAiPadel();
+}
+
+function updateBall () {
+  ball.top += ball.speed * Math.sin(ball.angle);
+  ball.left += ball.speed * Math.cos(ball.angle);
+  $ball.css({
+    top: `${ball.top}px`,
+    left: `${ball.left}px`
+  });
+
+  if (isBallOverlappingWithPlayerPadel()) {
+    if (ball.angle === UP_LEFT) {
+      ball.angle = UP_RIGHT;
+    } else {
+      ball.angle = DOWN_RIGHT;
+    }
+  }
+
+  if (isBallOverlappingWithAiPadel()) {
+    if (ball.angle === UP_RIGHT) {
+      ball.angle = UP_LEFT;
+    } else {
+      ball.angle = DOWN_LEFT;
+    }
+  }
+
+  if (isBallOverlappingWithTop()) {
+    if (ball.angle === UP_RIGHT) {
+      ball.angle = DOWN_RIGHT;
+    } else {
+      ball.angle = DOWN_LEFT;
+    }
+  }
+
+  if (isBallOverlappingWithBottom()) {
+    if (ball.angle === DOWN_RIGHT) {
+      ball.angle = UP_RIGHT;
+    } else {
+      ball.angle = UP_LEFT;
+    }
+  }
+}
+
+function isBallOverlappingWithPlayerPadel () {
+  return $ball.overlaps('#player-padel').length > 0
+}
+
+function isBallOverlappingWithAiPadel () {
+  return $ball.overlaps('#ai-padel').length > 0
+}
+
+function isBallOverlappingWithTop () {
+  return ball.top <= 0;
+}
+
+function isBallOverlappingWithBottom () {
+  return ball.top >= $pong.height() - $ball.height();
+}
+
+function updateAiPadel () {
   if (aiPadel.top > $pong.height() - $aiPadel.height()) {
     aiPadel.direction = -1;
   }
