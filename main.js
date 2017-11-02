2a75930155106af5ec877ca9f45620ae59f2cb83
Cody Seibert <cseibert113@gmail.com>
2017-11-02 17:00:00 -0400

    added functionality for moving the ai's padel

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const $pong = $('#pong');
 const $playerPadel = $('#player-padel');
+const $aiPadel = $('#ai-padel');
+
+const aiPadel = {
+  direction: 1,
+  SPEED: 3,
+  top: 0
+}
 
 $pong.mousemove(function (evt) {
   const top = Math.min(
@@ -10,3 +17,21 @@ $pong.mousemove(function (evt) {
     top: `${top}px`
   });
 });
+
+function update() {
+  if (aiPadel.top > $pong.height() - $aiPadel.height()) {
+    aiPadel.direction = -1;
+  }
+
+  if (aiPadel.top < 0) {
+    aiPadel.direction = 1;
+  }
+
+  aiPadel.top += aiPadel.direction * aiPadel.SPEED;
+  
+  $aiPadel.css({
+    top: `${aiPadel.top}px`
+  });
+}
+
+setInterval(update, 20);
